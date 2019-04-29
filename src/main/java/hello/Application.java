72c5e38de9d967ee42e6df41d54cd922c45ef5ff
geyuxu <github@geyuxu.cn>
2019-04-30 01:28:18 +0800

    上传图片

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,9 +1,13 @@
 package hello;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -20,4 +24,16 @@ public class Application implements CommandLineRunner {
         if(args.length > 0)
             calibra.doSth(args[0]);
     }
+
+//    @Bean
+//    public TomcatServletWebServerFactory tomcatEmbedded() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+//            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+//                //-1 means unlimited
+//                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+//            }
+//        });
+//        return tomcat;
+//    }
 }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,11 +3,18 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 import static hello.Calibra.jpgPathList;
 
@@ -49,6 +56,30 @@ public class GreetingController {
     public String index(@RequestParam(name="page", required=false, defaultValue="1") String page,Model model) {
         model.addAttribute("names", jpgPathList);
         model.addAttribute("page", page);
+        return "index";
+    }
+
+    @PostMapping("/upload")
+    public String index(@RequestParam("files") MultipartFile[] files,Integer idx,
+                        RedirectAttributes redirectAttributes) {
+        MultipartFile file = files[0];
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "index";
+        }
+
+        try {
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get("/Users/geyuxu/test/" + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "index";
     }
 }
